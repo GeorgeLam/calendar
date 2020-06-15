5cf6c04ef20cb43a45cf395c4cf6d1a0c50b83af
George <george@georgel.am>
2020-06-15 13:19:22 +0800

    Created branch for cleaning up

--- app.js
+++ app.js
@@ -11,80 +11,83 @@ let newAdded = 2;
 let inputs = ["A", "B"];
 
 btn.addEventListener("click", () => {
-    //text.innerHTML = ""
-    inputs.push(com.value);
-    
-     //for (let i = 0; i < inputs.length; i++) {
-         //console.log(inputs[newAdded])
-         text.innerHTML += `<li>
+  //text.innerHTML = ""
+  inputs.push(com.value);
+
+  //for (let i = 0; i < inputs.length; i++) {
+  //console.log(inputs[newAdded])
+  text.innerHTML += `<li>
                 <div id="list-text${newAdded}">${inputs[newAdded]}</div>
                 <div class="list"><a class="delBtn">
                     <i class="gg-close-o"></i></a>
                 <a class="editBtn">Edit</a>
                 </div>
-            </li>`
-         newAdded++;
-         console.log(text.innerHTML)
+            </li>`;
+  newAdded++;
+  console.log(text.innerHTML);
 
-   // }
-    delBtnEnable();
-    saveEditCheck();
-
-}
-);
+  // }
+  delBtnEnable();
+  saveEditCheck();
+});
+//Cleaned
 
 delBtnEnable = function () {
-    for (let i = 0; i < delBtn.length; i++){
-        delBtn[i].addEventListener("click", () => {
-            console.log(i + " " + delBtn.length)
-            if (i >= delBtn.length){i = delBtn.length -1; console.log("Correcting")} ////Corrected the penultimate error since the loop was prematurely ending otherwise. However, setting i to -1 or using <= delBtn.length initially would have caused errors, since would mean applying aEL to non-existent items -> causes the same error.
-            //inputs.pop();
-            delBtn[i].parentElement.parentElement.remove();
-            //if (i !== delBtn.length && i < 10){i++;};
-        }
-    )
-    }//end of for loop on delBtn
+  for (let i = 0; i < delBtn.length; i++) {
+    delBtn[i].addEventListener("click", () => {
+      console.log(i + " " + delBtn.length);
+      if (i >= delBtn.length) {
+        i = delBtn.length - 1;
+        console.log("Correcting");
+      } ////Corrected the penultimate error since the loop was prematurely ending otherwise. However, setting i to -1 or using <= delBtn.length initially would have caused errors, since would mean applying aEL to non-existent items -> causes the same error.
+      //inputs.pop();
+      delBtn[i].parentElement.parentElement.remove();
+      //if (i !== delBtn.length && i < 10){i++;};
+    });
+  } //end of for loop on delBtn
 
-
-    for (let i = 0; i < editBtn.length; i++){
-        editBtn[i].addEventListener("click", () => {
-            console.log(editBtn[i].parentElement.previousElementSibling.innerText)
-            editBtn[i].parentElement.previousElementSibling.innerHTML = `<input type="text" class="newText" value="${editBtn[i].parentElement.previousElementSibling.innerText}"><button class="saveEdit">Save Edit</button>`
-            //editBtn[i].parentElement.previousElementSibling.innerText = "";
-            saveEditCheck();
-        })
-    }
-
+  for (let i = 0; i < editBtn.length; i++) {
+    editBtn[i].addEventListener("click", () => {
+      console.log(editBtn[i].parentElement.previousElementSibling.innerText);
+      editBtn[
+        i
+      ].parentElement.previousElementSibling.innerHTML = `<input type="text" class="newText" value="${editBtn[i].parentElement.previousElementSibling.innerText}"><button class="saveEdit">Save Edit</button>`;
+      //editBtn[i].parentElement.previousElementSibling.innerText = "";
+      saveEditCheck();
+    });
+  }
 };
 
-let saveEditCheck = function() {
-    for (let i = 0; i < saveEdit.length; i++) {
+let saveEditCheck = function () {
+  for (let i = 0; i < saveEdit.length; i++) {
     saveEdit[i].addEventListener("click", () => {
-        if (i >= saveEdit.length) { i = saveEdit.length - 1; console.log("Correcting") }
-        console.log("You just clicked save on button " + i)
-        console.log(document.querySelectorAll("li").length);
+      if (i >= saveEdit.length) {
+        i = saveEdit.length - 1;
+        console.log("Correcting");
+      }
+      console.log("You just clicked save on button " + i);
+      console.log(document.querySelectorAll("li").length);
 
-        let savedNum = i
-        let numb = saveEdit[i].parentElement.parentElement.firstElementChild.id[saveEdit[i].parentElement.parentElement.firstElementChild.id.length-1]
-        
-        //editBtn[savedNum].parentElement.previousElementSibling.innerHTML = newTexts[savedNum].value;
-        document.querySelector(`#list-text${numb}`).innerHTML = newTexts[savedNum].value;
-    })
-}
-}
+      let savedNum = i;
+      let numb =
+        saveEdit[i].parentElement.parentElement.firstElementChild.id[
+          saveEdit[i].parentElement.parentElement.firstElementChild.id.length -
+            1
+        ];
+
+      //editBtn[savedNum].parentElement.previousElementSibling.innerHTML = newTexts[savedNum].value;
+      document.querySelector(`#list-text${numb}`).innerHTML =
+        newTexts[savedNum].value;
+    });
+  }
+};
 
 delBtnEnable();
 
+//text.innerHTML += `<li>${com.value}</li>`
+//adding to a list
+//inputs.push(com.value); text.appendChild(inputs) THIS just adds to single line, if txt is a p element.
 
-
-
-
-
-
-    //text.innerHTML += `<li>${com.value}</li>` 
-    //adding to a list
-    //inputs.push(com.value); text.appendChild(inputs) THIS just adds to single line, if txt is a p element.
-
-  //if (com.value === "delete"){
-    //text.innerHTML = text.innerHTML.slice(0, text.lastIndexOf("<li>"));
-    //inputs.pop(); text.innerHTML = inputs}
+//if (com.value === "delete"){
+//text.innerHTML = text.innerHTML.slice(0, text.lastIndexOf("<li>"));
+//inputs.pop(); text.innerHTML = inputs}
